1650d0451ad8711554dc5a5ac737e0c103cf53a7
Shantanu Jain <shantanu1227@gmail.com>
2020-04-04 03:43:46 +0530

    Upgrade/sdk37 (#5)
    * Upgraded to sdk 37
    
    * Removed google service key

--- app/components/NoDataRenderer.js
+++ app/components/NoDataRenderer.js
@@ -1,10 +1,23 @@
-import {Text} from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 import React from "react";
-import {colors} from "../styles/Common";
+import { colors } from "../styles/Common";
 
 export default function NoDataRenderer() {
 
-  return <Text style={colors.light}>
-    No tasks here
-  </Text>
+  return (
+    <View>
+      <Text style={style.text}>
+        No tasks here
+      </Text>
+    </View>
+  )
 }
+
+const style = StyleSheet.create({
+  text: {
+    ...colors.light,
+    fontSize: 24,
+    textAlign: 'center',
+    textAlignVertical: 'center',
+  }
+})
